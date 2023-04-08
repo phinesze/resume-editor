82f1ae07b87423aca2777b20d00ee570b93ad6e1
InoueShinichi <phinesze@gmail.com>
2023-04-08 20:12:37 +0900

    colSpanをnumber型にする

--- career-editor-frontend/components/organisms/CareerTable/DocumentBody.tsx
+++ career-editor-frontend/components/organisms/CareerTable/DocumentBody.tsx
@@ -10,10 +10,10 @@ export const DocumentBody = ({ label, document }: Props) => {
   return (
     <tbody>
       <tr>
-        <th className="p-2" colSpan="2">
+        <th className="p-2" colSpan={2}>
           {label}
         </th>
-        <td className="p-4 align-top" colSpan="3">
+        <td className="p-4 align-top" colSpan={3}>
           <MarkdownDocument document={document} />
         </td>
       </tr>
