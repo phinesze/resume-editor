3fe059dfd305dc06524c4c60b256a051b642d138
InoueShinichi <phinesze@gmail.com>
2023-04-21 14:29:03 +0900

    モックデータとして_mocksディレクトリをbackend/srcに作成

---
+++ resume-editor-backend/src/_mocks/ResumeDatas.ts
@@ -0,0 +1,40 @@
+import { ResumeData } from '@/types/ResumeData';
+
+const users: ResumeData[] = [
+  {
+    id: '1',
+    title: '職務経歴書1',
+    userId: '1',
+    data: {
+      caption: '職務経歴書',
+      lastUpdate: '2023-04-03',
+      works: [],
+      documents: [],
+    },
+    templateData: {},
+  },
+  {
+    id: '2',
+    title: '職務経歴書2',
+    userId: '1',
+    data: {
+      caption: '職務経歴書',
+      lastUpdate: '2023-04-13',
+      works: [],
+      documents: [],
+    },
+    templateData: {},
+  },
+  {
+    id: '3',
+    userId: '2',
+    title: 'resume_1',
+    data: {
+      caption: 'RESUME',
+      lastUpdate: '2023-04-15',
+      works: [],
+      documents: [],
+    },
+    templateData: {},
+  },
+];
---
+++ resume-editor-backend/src/_mocks/Users.ts
@@ -0,0 +1,14 @@
+import { User } from '@/types/User';
+
+const users: User[] = [
+  {
+    id: '1',
+    fullName: '降音　太郎',
+    resumeDataIds: ['1', '2'],
+  },
+  {
+    id: '2',
+    fullName: 'サンプル　データ',
+    resumeDataIds: ['3'],
+  },
+];
