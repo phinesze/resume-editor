84ff68a5e61ca5b501f8192788f49fe31fff2183
InoueShinichi <phinesze@gmail.com>
2023-04-21 18:00:01 +0900

    backendのProfileモジュール、コントローラ、サービスを作成

--- resume-editor-backend/src/app.module.ts
+++ resume-editor-backend/src/app.module.ts
@@ -4,10 +4,17 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ResumesModule } from './resumes/resumes.module';
 import { ComponentsModule } from './components/components.module';
+import { ProfilesService } from './profiles/profiles.service';
+import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ResumesModule, ComponentsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ResumesModule,
+    ComponentsModule,
+    ProfilesModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ProfilesService],
 })
 export class AppModule {}
